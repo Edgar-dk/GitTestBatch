09453623bd76d91114c46456532f46a35d0869e9
Edgar <Edgar@sias.com>
2022-12-26 21:01:56 +0800

    这个是Test01提交的T1

---
+++ src/main/java/T1.java
@@ -0,0 +1,10 @@
+/**
+ * @author Edgar
+ * @create 2022-12-26 20:59
+ * @faction:
+ */
+public class T1 {
+    public static void main(String[] args) {
+        System.out.println("这个是Test01的T1");
+    }
+}
